8f1ba6069f62d0ae5f6f0334104029b7b8e97941
santillienzo <enzo135246@gmail.com>
2023-11-01 10:44:04 -0300

    comentarios

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+//Selectores
 const porId = document.getElementById('header')
 const porClassName = document.getElementsByClassName('border-grey') //Devuelve un HTMLCollection
 const porTagName = document.getElementsByTagName('nav') //Devuelve un HTMLCollection
 const porName = document.getElementsByName('navegacion') //NodeList
 const queySelector = document.querySelector('#border-grey') //Devuelve solo uno
 const querySelectorAll = document.querySelectorAll('#border-grey') //NodeList
+
+/* 
+    La diferencia entre NodeList y HTMLColection es:
+        - HtmlColection: Listas dinámicas
+        - NodeList: Son listas estáticas
+*/
